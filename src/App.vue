24a1708591750ec9e9481c958f735824183b3282
unknown <abdiaziizarrale@gmail.com>
2022-01-27 19:49:11 -0800

    Koobsade App

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 
 <script setup lang="ts">
-import { RouterView,RouterLink} from 'vue-router';
-import Nav from "./components/Nav.vue";
-import Home from './components/pages/Home.vue';
-
+import Nav from "./components/pages/Nav.vue";
 
 </script>
 
-
-
-
 <template>
-<Nav /> 
-<Home/>
-  <RouterView />
+<Nav/>
 </template>
 
 
--- src/components/hel.vue
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-
-</script>
-<template>
-    <h1>hello</h1>
-</template>
